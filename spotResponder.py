8809ca25b3403278e8889e989fb4d7b12fb25486
Toufik Airane <toufik.airane@gmail.com>
2015-12-12 03:44:16 +0100

    spot

--- spotResponder.py
+++ spotResponder.py
@@ -21,7 +21,7 @@ def banner():
       `  `'--/  /---/*- -RESPONDER
             /  /
             `='     spotResponder.py <broadcast>
-            
+
 SpotResponder is an active scanner to detect Responder running on ur LAN
             """)
 
